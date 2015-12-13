93a292e596542e52ae0258b74c0c90544906df07
CoccoMarco <marco.cocconcelli@unimore.it>
2015-12-13 13:42:56 +0100

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,19 @@
+# Please note: this script loads a dataset called "household_power_consumption.txt"
+# that should be present in the working directory of your R session.
+#
+# Required package: lubridate
+
+# Load libraries
+require(lubridate)
+
+# Load data
+Data <- read.table("household_power_consumption.txt", header = TRUE,sep = ";", na.strings ="?", skipNul = TRUE, colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+Data$ExtendedTime <- dmy_hms(paste(Data[,1],"",Data[,2]))
+Data[,1] <- dmy(Data[,1])
+Data[,2] <- hms(Data[,2])
+Data <- Data[Data[,1]==dmy("01/02/2007")|Data[,1]==dmy("02/02/2007"),]
+
+# Plot 1
+png(file = "Plot1.png", width = 480, height = 480, units = "px") 
+hist(Data$Global_active_power,col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
